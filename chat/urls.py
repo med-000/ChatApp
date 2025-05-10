15311dbc1f3a5471d268d3797f3278bff3d18e8d
med000 <med.rk000@gmail.com>
2025-05-10 12:49:10 +0900

    ログイン機能と新規登録機能の実装

--- chat/urls.py
+++ chat/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('<str:room>/', views.room, name='room'),
+    path('',views.login,name='login'),
+    path('register/',views.register, name = 'register'),
+    path('home', views.home, name='home'),
+    path('home/<str:room>/', views.room, name='room'),
     path('checkview', views.checkview, name='checkview'),
     path('send', views.send, name='send'),
     path('getMessages/<str:room>/', views.getMessages, name='getMessages'),
--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,55 @@
 from django.shortcuts import render, redirect
 from chat.models import Room, Message
 from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+import os
+
 
 # Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username = username, password = password )
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            messages.info(request,'ログイン情報が間違っています')
+            return redirect('login')
+    
+    else:
+        return render(request, "login.html")
+    
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "メールが既に使われています")
+                return redirect('register')
+            elif User.objects.filter(username = username).exists():
+                messages.info(request, "ユーザー名が既に使われています")
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username = username , email = email , password = password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'パスワードが一致しません')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+
 def home(request):
     return render(request, 'home.html')
 
