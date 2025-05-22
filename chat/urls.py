49d71dec93132ea291169333f868b51d11abf24d
med000 <med.rk000@gmail.com>
2025-05-22 17:24:19 +0900

    修正

--- chat/urls.py
+++ chat/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.index,name = 'index')
+    path('',views.index,name = 'index'),
     path('login/',views.login,name='login'),
     path('register/',views.register, name = 'register'),
     path('mypage/<str:username>/',views.mypage, name = 'mypage'),
